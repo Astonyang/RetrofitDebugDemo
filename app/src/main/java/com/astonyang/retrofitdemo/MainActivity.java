15e9193a8123ae6eb765d0a3db0fe1b551cef047
yangxl <yangxl@mail.jj.cn>
2018-04-28 19:00:14 +0800

    init code---1

--- app/src/main/java/com/astonyang/retrofitdemo/MainActivity.java
+++ app/src/main/java/com/astonyang/retrofitdemo/MainActivity.java
@@ -3,6 +3,9 @@ package com.astonyang.retrofitdemo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.io.IOException;
 
 /**
  * Created by yangxl on 2018/4/28.
@@ -13,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSimpleService(View view) {
+        try {
+            SimpleService.main();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
